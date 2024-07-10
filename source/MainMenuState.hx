72e6d387ffb5835bf4331ed3a204afd4cdf7c6d4
Idk1245 <93317189+Idk1245@users.noreply.github.com>
2024-07-10 21:41:32 +0100

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -177,6 +177,8 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollow, null, 0.60 * (60 / FlxG.save.data.fpsCap));
 
+		var credits:FlxText = new FlxText(FlxG.width - 300, FlxG.height - 18 * 2, 300, "Android Port By MaysLastPlay, MarioMaster, Stefan2008, Ralsei", 3);
+		credits.scrollFactor.set();
 		credits.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(credits); //If you reading this, I'm putting random comments on my code cuz i feel lonely.
 
@@ -203,7 +205,7 @@ class MainMenuState extends MusicBeatState
 				if (urmom == 30)
 				{
 					FlxG.save.data.storyProgress = 0;
-					FlxG.save.data.soundTestUnlocked = true;
+					FlxG.save.data.soundTestUnlocked = false;
 					FlxG.save.data.songArray = [];
 					FlxG.switchState(new MainMenuState());
 				}
