b0e40e50ec12b84d02f46e3fb1caa1a2c25ae99e
Idk1245 <93317189+Idk1245@users.noreply.github.com>
2024-07-14 00:06:26 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2265,7 +2265,7 @@ class PlayState extends MusicBeatState
 					camHUD.alpha = 0;
 					startCountdown();
 
-				case 'too-slow' | 'you-cant-run' | 'triple-trouble' | 'endless' | 'endless-og' | 'endeavours' | 'cycles' | 'malediction' | 'prey' | 'fight-or-flight'| 'substantial' | 'digitalized' | 'round-a-bout' | 'relax' | 'hedge' | 'manual-blast' | 'shocker' | 'hollow' | 'b4cksl4sh':
+				case 'too-slow' | 'you-cant-run' | 'you-cant-run_encore' | 'triple-trouble' | 'endless' | 'endless-og' | 'endeavours' | 'cycles' | 'malediction' | 'prey' | 'fight-or-flight'| 'substantial' | 'digitalized' | 'round-a-bout' | 'relax' | 'hedge' | 'manual-blast' | 'shocker' | 'hollow' | 'b4cksl4sh':
 
 					if (daSong == 'too-slow' || daSong == 'you-cant-run' || daSong == 'cycles')
 						{
