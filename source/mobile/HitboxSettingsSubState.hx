f8f73e12b92f6c1c2239a9abd7b6d63ba60c6568
Idk1245 <93317189+Idk1245@users.noreply.github.com>
2024-07-24 21:31:30 +0100

    Update HitboxSettingsSubState.hx

--- source/mobile/HitboxSettingsSubState.hx
+++ source/mobile/HitboxSettingsSubState.hx
@@ -48,3 +48,24 @@ class HitboxSettingsSubState extends BaseOptionsMenu
 		super();
 	}
 }
+
+class GradientHitboxes extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		FlxG.save.data.gradientHitboxes = !FlxG.save.data.gradientHitboxes;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return (FlxG.save.data.gradientHitboxes ? "Gradient Hitboxes On" : "Gradient Hitboxes Off");
+	}
+}
